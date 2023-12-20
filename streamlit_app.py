b9e99188c69c924f22de33191196c1a50f5dea9e
Danis Nurmansyah <danisnurman@users.noreply.github.com>
2023-12-21 00:24:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
-import pandas as pd
+print("Success")
+# import pandas as pd
 
-train = pd.read_csv('diabetes_binary_5050split_health_indicators_BRFSS2015.csv')
-train.isnull().sum()
+# train = pd.read_csv('diabetes_binary_5050split_health_indicators_BRFSS2015.csv')
+# train.isnull().sum()
 
-train.dropna(inplace=True)
-train.isnull().sum()
+# train.dropna(inplace=True)
+# train.isnull().sum()
 
-train.head(10)
+# train.head(10)
